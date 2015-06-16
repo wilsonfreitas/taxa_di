5bc06848b21816eab87a80d84180d19492708449
wilsonfreitas <wilson.freitas@gmail.com>
2015-06-15 22:21:27 -0300

    Implemented scraper to download data from cetip

--- scraper.py
+++ scraper.py
@@ -1,20 +1,33 @@
 # This is a template for a Python scraper on morph.io (https://morph.io)
 # including some code snippets below that you should find helpful
 
-# import scraperwiki
-# import lxml.html
-#
-# # Read in a page
-# html = scraperwiki.scrape("http://foo.com")
-#
-# # Find something on the page using css selectors
-# root = lxml.html.fromstring(html)
-# root.cssselect("div[align='left']")
-#
-# # Write out to the sqlite database using scraperwiki library
-# scraperwiki.sqlite.save(unique_keys=['name'], data={"name": "susan", "occupation": "software developer"})
-#
-# # An arbitrary query against the database
+import scraperwiki
+import lxml.html
+
+import textparser as tp
+
+class DateParser(tp.PortugueseRulesParser):
+    def parseDate(self, text, match):
+        r'\((\d\d)/(\d\d)/(\d\d\d\d)\)'
+        return '{0}-{1}-{2}'.format(match.group(3), match.group(2), match.group(1))
+
+
+parser = DateParser()
+
+# Read in a page
+html = scraperwiki.scrape("http://www.cetip.com.br")
+
+# Find something on the page using css selectors
+root = lxml.html.fromstring(html)
+date = root.cssselect("#ctl00_Banner_lblTaxDateDI")[0].text_content().strip()
+date = parser.parse(date)
+rate = root.cssselect("#ctl00_Banner_lblTaxDI")[0].text_content().strip().replace('%', '')
+rate = parser.parse(rate)
+
+# Write out to the sqlite database using scraperwiki library
+scraperwiki.sqlite.save(unique_keys=['date'], data={"date": date, "rate": rate})
+
+# An arbitrary query against the database
 # scraperwiki.sql.select("* from data where 'name'='peter'")
 
 # You don't have to do things with the ScraperWiki and lxml libraries.
